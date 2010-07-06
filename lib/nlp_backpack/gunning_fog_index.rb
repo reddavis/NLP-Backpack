1e227e825850ff95d574dfc1a48233995ea4400d
reddavis <reddavis@gmail.com>
2010-07-06 23:16:46 +0200

    gunning fog index docs

--- lib/nlp_backpack/gunning_fog_index.rb
+++ lib/nlp_backpack/gunning_fog_index.rb
@@ -3,28 +3,38 @@ require 'nlp_backpack/tokenizers/sentence'
 require 'nlp_backpack/syllable/flesch_index'
 
 module NLPBackpack
-
+  # = Gunning Fog Index
+  # Example:
+  #
+  # text = "the cat sat on the mat"
+  # gfi = NLPBackpack::GunningFogIndex.new(text)
+  # gfi.process => the answer
   class GunningFogIndex
     def initialize(text)
       @text = text
     end
 
+    # Return the GFI
     def process
       0.4 * ( (word_count / sentence_count) + 100 * (complex_word_count / word_count) )
     end
 
+    # The average sentence length of the inputted text
     def average_sentence_length
       word_count / sentence_count
     end
 
+    # Overall word count
     def word_count
       @word_count ||= tokenized_words.size.to_f
     end
 
+    # Overall sentence count
     def sentence_count
       @sentence_count ||= Tokenizer::Sentence.tokenize(@text).size.to_f
     end
 
+    # Overall complex word count (words >= 3 syllables)
     def complex_word_count
       @complex_word_count ||= Syllable::FleschIndex.new(*tokenized_words).process.delete_if {|word, s_count| s_count < 3}.size.to_f
     end
